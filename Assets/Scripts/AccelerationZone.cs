2a1eb8711fb898bd5409598969acdc38d273307d
Bebo-K <Bebo.Kimble@gmail.com>
2024-01-28 10:44:30 -0500

    Finished acceleration zone, seesaws reset on death

--- Assets/Scripts/AccelerationZone.cs
+++ Assets/Scripts/AccelerationZone.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class AccelerationZone : MonoBehaviour
 {
 
+    [SerializeField] public Transform direction;
+    private Vector3 boost;
+    private List<GameObject> boostedObjects;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        boostedObjects = new List<GameObject>();
+        boost =  (direction.position - this.transform.position);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        foreach (GameObject obj in boostedObjects) {
+            obj.GetComponent<Rigidbody2D>().AddForce(boost);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ball"))
+        {
+            Debug.Log("Ball being boosted");
+            boostedObjects.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Ball stopped being boosted");
+        boostedObjects.Remove(collision.gameObject);
     }
 }
--- Assets/Scripts/SeesawCOMBias.cs
+++ Assets/Scripts/SeesawCOMBias.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SeesawCOMBias : MonoBehaviour
 {
-    public float bias;
+    [SerializeField] PlayerInput playerController;
+    [SerializeField] public float bias;
     private Rigidbody2D rigidbody2D;
+
+    private Quaternion initialRotation;
 
     void Start() {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        initialRotation = this.transform.rotation;
     }
+
+    private void OnEnable()
+    {
+        PlayerController.OnRespawn += OnReset;
+    }
+
+    private void OnReset(PlayerController playerController)
+    {
+        rigidbody2D.angularVelocity = 0;
+        transform.rotation = initialRotation;
+    }
+
 
     private void FixedUpdate()
     {
