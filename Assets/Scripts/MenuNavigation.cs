726dab3e682dadce2c2d4e6713a5302464ec180a
armandpreschutti <armand.preschutti@gmail.com>
2024-01-31 00:51:29 -0500

    Imported TMP assets and Redesigned Main Menu

--- Assets/Scripts/MenuNavigation.cs
+++ Assets/Scripts/MenuNavigation.cs
@@ -6,23 +6,23 @@ using UnityEngine.SceneManagement;
 public class MenuNavigation : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] public GameObject menuStuff;
-    [SerializeField] public GameObject creditsStuff;
+    [SerializeField] public GameObject menuPanel;
+    [SerializeField] public GameObject creditsPanel;
 
     public void OnPlayButton() {
         SceneManager.LoadScene("ClownCollegeLayout");
     
     }
     public void OnCreditsButton() {
-        menuStuff.SetActive(false);
-        creditsStuff.SetActive(true);
+        menuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
 
     }
     public void OnCreditsBackButton()
     {
         Debug.Log("back button pressed");
-        creditsStuff.SetActive(false);
-        menuStuff.SetActive(true);
+        creditsPanel.SetActive(false);
+        menuPanel.SetActive(true);
     }
 
     public void OnExitButton(){
